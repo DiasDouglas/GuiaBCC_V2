3ce7037a533998a026ee4e5b7f4f76324fb419ec
IsmaelCesar <leamscesar@gmail.com>
2018-01-22 18:27:43 -0300

    Adicionando padrão view holder

--- Guia-bcc-ufrpe-master/Guia-BCC/app/src/main/java/adapters/ListaDeDisciplinasCursadasAdapter.java
+++ Guia-bcc-ufrpe-master/Guia-BCC/app/src/main/java/adapters/ListaDeDisciplinasCursadasAdapter.java
@@ -47,26 +47,53 @@ public class ListaDeDisciplinasCursadasAdapter extends BaseAdapter {
         return 0;
     }
 
+    @SuppressLint("RestrictedApi")
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         /*Implementando conceitos aprendidos na aula sobre list views*/
         //Implementacoes quanto a o convertView ainda serão levados em consideração
         //O formato foi diferente pois estou lidando com um  Fragment e não com uma activity
-        @SuppressLint("RestrictedApi") View myView =  act.getLayoutInflater(savedInstanceState).inflate(R.layout.item_lista_disciplina,parent,false);
+         View myView;
+        ViewHolder holder;
+        TextView tvNomeDisciplina;
+        TextView qtdItens;
+        TextView ultimaAtt;
+        TextView tvAvaliacaoGeral;
 
         DisciplinaDTO disc = listaDisciplinas.get(position);
 
-        TextView tvNomeDisciplina = (TextView) myView.findViewById(R.id.tvNomeDisciplina);
-        TextView qtdItens = (TextView) myView.findViewById(R.id.tvQtdItens);
-        TextView ultimaAtt = (TextView) myView.findViewById(R.id.tvUltimaAtt);
-        TextView tvAvaliacaoGeral = (TextView) myView.findViewById(R.id.tvAvaliacaoGeral);
+        if(convertView == null) {
+            myView =  act.getLayoutInflater(savedInstanceState).inflate(R.layout.item_lista_disciplina,parent,false);
+            holder = new ViewHolder(myView);
 
-        tvNomeDisciplina.setText(disc.getNome());
-        qtdItens.setText(String.valueOf(disc.getQtdItens()));
-        ultimaAtt.setText(disc.getUltimaAtt());
-        tvAvaliacaoGeral.setText(String.valueOf(disc.getAvaliacaoGeral()));
+            holder.tvNomeDisciplina.setText(disc.getNome());
+            holder.qtdItens.setText(String.valueOf(disc.getQtdItens()));
+            holder.ultimaAtt.setText(disc.getUltimaAtt());
+          holder.tvAvaliacaoGeral.setText(String.valueOf(disc.getAvaliacaoGeral()));
+
+        }
+        else {
+            myView = convertView;
+            holder = (ViewHolder) myView.getTag();
+        }
 
         return myView;
     }
+
+    class ViewHolder {
+
+        TextView tvNomeDisciplina;
+        TextView qtdItens;
+        TextView ultimaAtt;
+        TextView tvAvaliacaoGeral;
+
+        ViewHolder(View myView) {
+            tvNomeDisciplina = (TextView) myView.findViewById(R.id.tvNomeDisciplina);
+            qtdItens = (TextView) myView.findViewById(R.id.tvQtdItens);
+            ultimaAtt = (TextView) myView.findViewById(R.id.tvUltimaAtt);
+            tvAvaliacaoGeral = (TextView) myView.findViewById(R.id.tvAvaliacaoGeral);
+        }
+    }
+
 }
 
