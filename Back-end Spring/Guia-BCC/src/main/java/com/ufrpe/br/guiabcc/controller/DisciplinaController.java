3ea0e408ce7eaf751fcd4b8333ca3a7b725b51ef
Fabio Mariano <fabiio.mariiano@gmail.com>
2018-01-29 21:12:33 -0300

    adicionando novo método no repositorio de disciplinas

--- Back-end Spring/Guia-BCC/src/main/java/com/ufrpe/br/guiabcc/controller/DisciplinaController.java
+++ Back-end Spring/Guia-BCC/src/main/java/com/ufrpe/br/guiabcc/controller/DisciplinaController.java
@@ -20,7 +20,7 @@ import com.ufrpe.br.guiabcc.repository.DisciplinaRepository;
 import com.ufrpe.br.guiabcc.repository.ProfessorRepository;
 
 @RestController
-@RequestMapping("guiabcc")
+@RequestMapping("/guiabcc")
 public class DisciplinaController {
 	
 	@Autowired
@@ -107,7 +107,7 @@ public class DisciplinaController {
 	//front-end
 	@GetMapping(path = "/disciplinaDto")
 	public List<DisciplinaDTO> listAllDisciplinaDTO(){
-		List<Disciplina> discs = disciplinaRepository.findAll();
+		List<Disciplina> discs = disciplinaRepository.findAllByOrderByNomeDisciplina();
 		List<DisciplinaDTO> discsDTO = new ArrayList<>();
 		for (Disciplina disciplina : discs) {
 			discsDTO.add(new DisciplinaDTO(disciplina));
